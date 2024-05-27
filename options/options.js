10fdac9de5eb1ae4a2dce3da34c0d9240def9cf9
Crowdbotics <team@crowdbotics.com>
2024-05-27 09:44:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nest-minded-48118",
+  url: "https://nest-minded-48118.botics.co",
+  api: "https://nest-minded-48118.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
